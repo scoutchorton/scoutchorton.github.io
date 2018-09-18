1d22f15822d1ca5abac4d69efd258b05fc1d7b5a
scoutchorton <scoutchorton@users.noreply.github.com>
2018-09-18 11:02:39 -0400

    Transparent Background

--- clock/rotScripts.js
+++ clock/rotScripts.js
@@ -15,36 +15,24 @@ function getTime(v=0){
     a="P.M.";
   }
   if(v===3){
-    d[1]=("0"+(d[1]+1)).slice(-2);
-    d[2]=("0"+(d[2]+1)).slice(-2);
+    d[1]=("0"+(d[1])).slice(-2);
+    d[2]=("0"+(d[2])).slice(-2);
     return([d,a]);
   }
   //Convert to radians
-  d[0]=Math.radians((d[0]*3)-90);
+  d[0]=Math.radians((d[0]*30)-90);
   d[1]=Math.radians((d[1]*6)-90);
   d[2]=Math.radians((d[2]*6)-90);
   //End
   return(d[v]);
 }
 window.onload=function(){
-	let h=eId('c').height=eId('c').width=document.body.clientHeight;let w=h;
+	let h=eId('c').style.height=eId('c').style.width=document.body.clientHeight;let w=h;
   setInterval(function(){
     let c=eId('c').getContext('2d');
     c.beginPath();
-    //Background
-    c.fillStyle="#ffffff";
-    c.fillRect(0,0,w,h);
-    c.closePath();
-    
-    //Hour
-    c.beginPath();
-    c.strokeStyle="#be9d31";
-    c.moveTo(w/2,h/2);
-    c.translate(w/2,w/2);
-    c.rotate(getTime());
-    c.lineTo(w/2,0);
-    c.setTransform(1, 0, 0, 1, 0, 0);
-    c.stroke();c.closePath();
+    //Clear
+    c.clearRect(0,0,w,h);
     
     //Minutes
     c.beginPath();
@@ -66,6 +54,24 @@ window.onload=function(){
     c.setTransform(1, 0, 0, 1, 0, 0);
     c.stroke();c.closePath();
     
+    //Hour
+    c.beginPath();
+    c.strokeStyle="#be9d31";
+    c.moveTo(w/2,h/2);
+    c.translate(w/2,w/2);
+    c.rotate(getTime());
+    c.lineTo((3*(w/2))/4,0);
+    c.setTransform(1, 0, 0, 1, 0, 0);
+    c.stroke();c.closePath();
+    
+    //Outline
+    c.beginPath();
+    c.lineWidth=2;
+    c.strokeStyle="#000000";
+    c.arc(w/2,h/2,(h/2)-2,0,2*Math.PI);
+    c.stroke();
+    c.closePath();
+    
     //Text
     c.beginPath();
     c.fillStyle="#000000";
@@ -76,14 +82,6 @@ window.onload=function(){
     c.textAlign="center";
     c.font=w/7.5+"px Cursive";
     c.fillText(m,(w/2),h/2);
-    c.closePath();
-    
-    //Outline
-    c.beginPath();
-    c.lineWidth=2;
-    c.strokeStyle="#000000";
-    c.arc(w/2,h/2,(h/2)-2,0,2*Math.PI);
-    c.stroke();
     c.closePath();
   },1/60*1000);
 };
